597aba7d2925cee0f1ebf2a84a34873147169bc4
lll <2418759512@qq.com>
2025-11-03 17:29:35 +0800

    斗笠提升

---
+++ GUILayout/npc/41.lua
@@ -0,0 +1,73 @@
+--npc名称：
+--npc功能：
+local npc = {}
+
+npc._config = teshudata["npc_41"]
+
+function npc.main(npcid, p2, p3, msgData)
+
+    local function UI_updata(node) --界面渲染
+
+        GUI:removeAllChildren(node)
+
+        local item = SL:GetMetaValue("EQUIP_DATA", npc._config.where)
+        if item then
+
+
+            local kuang = GUI:Image_Create(node, "kuang2", 400, 250, "res/wy/public/70_70_k.png")
+            UiTools.showItemData(kuang, SL:GetMetaValue("ITEM_DATA",SL:GetMetaValue("ITEM_INDEX_BY_NAME",npc._config.give)))
+
+            GUI:Text_Create(node, "wz5",200,200, 20, "#FF0000", "消耗:")
+            local cost_show = ItemNumByTable_img(npc._config.cost, nil,GUI:Node_Create(node, "cost_show", 0, 0))
+            GUI:setPosition(cost_show, 200, 130)
+
+        end
+        local Button= GUI:Button_Create(node, "Button", 750, 100.00, "res/public/1900000660.png")
+        GUI:Button_setTitleText(Button, "升级")
+        GUI:Button_setTitleFontSize(Button, 14)
+
+        GUI:addOnClickEvent(Button, function()
+            SL:SendLuaNetMsg(100, npcid, 1, 0, "")
+        end)
+
+
+    end
+
+
+    if p2 == 0 then--界面
+        npc.data = SL:JsonDecode(msgData,false)
+        local parent = GUI:GetWindow(nil, "npc_" .. npcid)
+        if parent then
+            GUI:removeAllChildren(parent)
+            GUI:setPosition(parent, cogin.w / 2, cogin.h / 2)
+        else
+            parent = GUI:Win_Create("npc_" .. npcid, cogin.w / 2, cogin.h / 2, 0, 0, false, false, true, true, true, npcid, 1)
+        end
+        local bjt = GUI:Image_Create(parent, "bjt", 0, 0, "res/public/1900000651_1.png")
+        GUI:setAnchorPoint(bjt, 0.5, 0.5)
+        GUI:setContentSize(bjt, cogin.w + 100, cogin.h + 100)
+        GUI:setTouchEnabled(bjt, true)
+        GUI:addOnClickEvent(bjt, function()
+            GUI:Win_Close(parent)
+        end)
+        GUI:addMouseOverTips(bjt, "", {x = 0, y = 0}, {x = 0, y = 0})
+
+        npc.bg = GUI:Image_Create(parent, "img_bj", 0, 0, 'res/wy/public/01.png')
+        GUI:setAnchorPoint(npc.bg, 0.5, 0.5)
+        GUI:setTouchEnabled(npc.bg, true)
+        GUI:Timeline_Window1(npc.bg)
+
+        local close = GUI:Button_Create(npc.bg, 'close', 930, 480, 'res/wy/public/close.png')
+        GUI:addOnClickEvent(close, function()
+            GUI:Win_Close(parent)
+        end)
+
+        npc.node = GUI:Node_Create(npc.bg, "node", 0, 0)
+        UI_updata(npc.node)
+    elseif p2 == 1 then
+        npc.data.level = npc.data.level + 1
+        UI_updata(npc.node)
+    end
+end
+
+return npc
