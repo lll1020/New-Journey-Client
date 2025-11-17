17480a98ed2ddc91e8bb9e995784217feb485e88
lll <2418759512@qq.com>
2025-11-17 21:46:58 +0800

    新增npc功能，支持任务领取与完成，优化界面渲染逻辑

---
+++ GUILayout/npc/55.lua
@@ -0,0 +1,74 @@
+--npc名称：
+--npc功能：
+local npc = {}
+
+npc._config = {}
+
+function npc.main(npcid, p2, p3, msgData)
+
+    local function UI_updata(node) --界面渲染
+        GUI:removeAllChildren(node)
+
+
+
+
+        GUI:setAnchorPoint(
+                GUI:RichText_Create(node, "desc", 200, 430,
+                        "<font color='#00FF00' size='20' >领取任务 共计击杀200只怪物，当前击杀："..(npc.data.sg_data.npc55 or 0).."</font>"
+                , 500, 20, "#f7f7de", 3,nil,nil,{outlineSize = 2,outlineColor = SL:ConvertColorFromHexString("#100808")})
+        , 0, 1)
+
+
+        local Button= GUI:Button_Create(node, "Button", 750, 100.00, "res/public/1900000660.png")
+        GUI:Button_setTitleText(Button, "领取任务")
+        GUI:Button_setTitleFontSize(Button, 14)
+
+        GUI:addOnClickEvent(Button, function()
+            SL:SendLuaNetMsg(100, npcid, 1, 0, "")
+        end)
+
+
+        Button= GUI:Button_Create(node, "Button2", 750, 150.00, "res/public/1900000660.png")
+        GUI:Button_setTitleText(Button, "完成任务")
+        GUI:Button_setTitleFontSize(Button, 14)
+
+        GUI:addOnClickEvent(Button, function()
+            SL:SendLuaNetMsg(100, npcid, 2, 0, "")
+        end)
+    end
+
+
+    if p2 == 0 then--界面
+        npc.data = SL:JsonDecode(msgData,false)
+        local parent = GUI:GetWindow(nil, "npc_" .. npcid)
+        if parent then
+            GUI:removeAllChildren(parent)
+            GUI:setPosition(parent, cogin.w / 2, cogin.h / 2)
+        else
+            parent = GUI:Win_Create("npc_" .. npcid, cogin.w / 2, cogin.h / 2, 0, 0, false, false, true, true, true, npcid, 1)
+        end
+        local bjt = GUI:Image_Create(parent, "bjt", 0, 0, "res/public/1900000651_1.png")
+        GUI:setAnchorPoint(bjt, 0.5, 0.5)
+        GUI:setContentSize(bjt, cogin.w + 100, cogin.h + 100)
+        GUI:setTouchEnabled(bjt, true)
+        GUI:addOnClickEvent(bjt, function()
+            GUI:Win_Close(parent)
+        end)
+        npc.bg = GUI:Image_Create(parent, "img_bj", 0, 0, 'res/wy/public/01.png')
+        GUI:setAnchorPoint(npc.bg, 0.5, 0.5)
+        GUI:setTouchEnabled(npc.bg, true)
+        GUI:Timeline_Window1(npc.bg)
+
+        local close = GUI:Button_Create(npc.bg, 'close', 930, 480, 'res/wy/public/close.png')
+        GUI:addOnClickEvent(close, function()
+            GUI:Win_Close(parent)
+        end)
+
+        npc.node = GUI:Node_Create(npc.bg, "node", 0, 0)
+        UI_updata(npc.node)
+    elseif p2 == 1 then
+        UI_updata(npc.node)
+    end
+end
+
+return npc
